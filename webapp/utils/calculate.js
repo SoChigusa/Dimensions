252f5140fe3140111d392da6008145c37517106a
So Chigusa <so.chigusa.pp@gmail.com>
2023-02-12 18:02:09 -0800

    post errors for eval of NaN

--- webapp/utils/calculate.js
+++ webapp/utils/calculate.js
@@ -6,7 +6,7 @@ const consteV = ({ extractConstantInfo, input, addAlerts }) => {
   if (constantInfo.isAbsent) {
     addAlerts({
       severity: 'error',
-      content: 'Constant name "' + input + '" does not match any constant in the database!'
+      content: 'Constant "' + input + '" is not a number and does not match any constant in the database!'
     });
   } else {
     value = constantInfo.value;
@@ -23,6 +23,11 @@ const uniteV = ({ extractUnitInfo, input, addAlerts }) => {
       addAlerts({
         severity: 'error',
         content: 'Unit name "' + unit.name + '" does not match any unit in the database!'
+      });
+    } else if (isNaN(unit.power)) {
+      addAlerts({
+        severity: 'error',
+        content: 'Unit power "' + unit.power + '" is not a number!'
       });
     } else {
       value *= (unitInfo.prefix.value * unitInfo.unit.value) ** eval(unit.power);
@@ -42,10 +47,25 @@ const calculate = ({ extractConstantInfo, extractUnitInfo, output, input, option
     const ineV = parameter.units.length == 0 ?
       consteV({ extractConstantInfo, input: parameter.name, addAlerts }) : // constant
       uniteV({ extractUnitInfo, input: parameter.units, addAlerts }); // parameter
-    const power = index == 0 ? -eval(parameter.power) : eval(parameter.power); // only difference btw output & input
-    value *= (eval(parameter.value) * ineV.value) ** power;
-    dimension += ineV.dimension * power;
-    if (index == 0) outputDimension = -dimension;
+    if (!isNaN(parameter.value) && !isNaN(parameter.power)) {
+      const power = index == 0 ? -eval(parameter.power) : eval(parameter.power); // only difference btw output & input
+      value *= (eval(parameter.value) * ineV.value) ** power;
+      dimension += ineV.dimension * power;
+      if (index == 0) outputDimension = -dimension;
+    } else {
+      if (isNaN(parameter.value)) {
+        addAlerts({
+          severity: 'error',
+          content: 'Parameter value "' + parameter.value + '" is not a number!'
+        });
+      }
+      if (isNaN(parameter.power)) {
+        addAlerts({
+          severity: 'error',
+          content: 'Parameter power "' + parameter.power + '" is not a number!'
+        });
+      }
+    }
   });
 
   if (dimension != 0) {
